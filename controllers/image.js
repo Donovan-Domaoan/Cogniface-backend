15b2306e3e0c454e05197abf5233b34d2fbfaa02
Donovan D <lucasdomaoan@gmail.com>
2025-04-24 00:40:43 -0700

    added all files for backend

---
+++ controllers/image.js
@@ -0,0 +1,63 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 37735c1bffb3434b82929784e60ded0a");
+
+const handleApiCall = (req, res) => {
+    const { input } = req.body;
+
+    stub.PostModelOutputs(
+        {
+            model_id: "face-detection",
+            user_app_id: {
+                user_id: "donovandomaoan",
+                app_id: "face-recognize"
+            },
+            inputs: [
+                {
+                    data: {
+                        image: {
+                            url: input
+                        }
+                    }
+                }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                return res.status(400).json("API error: " + err);
+            }
+
+            if (response.status.code !== 10000) {
+                return res.status(400).json("Clarifai failure: " + response.status.description);
+            }
+
+            res.json(response); 
+        }
+    );
+};
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db("users")
+        .where("id", "=", id)
+        .increment("entries", 1)
+        .returning("*")
+        .then(users => {
+            if (users.length) {
+                res.json(users[0].entries);
+            } else {
+                res.status(400).json('unable to display user')
+            }
+        })
+        .catch(err => res.status(400).json("unable to get entries count"));
+};
+
+module.exports = {
+    handleApiCall,
+    handleImage
+};
+
