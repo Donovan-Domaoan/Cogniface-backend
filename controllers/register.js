366a7e0825005588cb189298501e65dbc6d75d85
Donovan D <lucasdomaoan@gmail.com>
2025-04-25 00:16:01 -0700

    new

--- controllers/register.js
+++ controllers/register.js
@@ -1,18 +1,15 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    console.log("registration received:", req.body);
-    console.log("BODY RECEIVED:", req.body);
-
+    console.log("Registration received:", req.body);
     const { email, name, password } = req.body;
 
     if (!email || !name || !password) {
         console.log('Missing fields');
-        return res.status(400).json({ error: 'incorrect form submission' });
+        return res.status(400).json('incorrect form submission');
     }
 
     const hash = bcrypt.hashSync(password);
 
     db.transaction(trx => {
-        console.log('Starting transaction...');
         trx.insert({
             hash: hash,
             email: email
@@ -20,7 +17,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .into('login')
         .returning('email')
         .then(loginEmail => {
-            console.log('Inserted into login:', loginEmail);
+            console.log('Login insert returned:', loginEmail);
             return trx('users')
                 .returning('*')
                 .insert({
@@ -28,28 +25,29 @@ const handleRegister = (req, res, db, bcrypt) => {
                     name: name,
                     joined: new Date(),
                     entries: 0
-                })
-                .then(user => {
-                    console.log('Inserted into users:', user);
-                    if (user && user[0]) {
-                        return res.json(user[0]);
-                    } else {
-                        console.log("no user returned from DB")
-                        res.status(500).json({ error: 'User creation failed' });
-                    }
                 });
         })
+        .then(user => {
+            console.log('User insert returned:', user);
+            if (user && user[0]) {
+                res.json(user[0]);
+            } else {
+                res.status(500).json({ error: 'User creation failed' });
+            }
+        })
         .then(trx.commit)
-        .catch(error => {
-            console.error('Transaction failed:', error);
+        .catch(err => {
+            console.error('Transaction failed, rolling back:', err);
             trx.rollback();
-            res.status(500).json({ error: 'Transaction error' });
+            res.status(500).json({ error: 'Transaction failed' });
         });
-    }).catch(err => {
-        console.error('Registration error:', err);
-        res.status(400).json({ error: "unable to register" });
+    })
+    .catch(err => {
+        console.error('Outer error in registration:', err);
+        res.status(400).json("unable to register");
     });
-};
+}
+
  
 module.exports = {
     handleRegister
