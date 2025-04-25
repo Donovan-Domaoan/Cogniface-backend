28cee2b5c4accab1a38e23c93ec844af926a632f
Donovan D <lucasdomaoan@gmail.com>
2025-04-25 01:01:10 -0700

    new

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,36 @@
- const handleSignin = (req, res, db, bcrypt) => {
-    const {email, password} = req.body;
+const handleSignin = (req, res, db, bcrypt) => {
+    console.log('Signin request received:', req.body);
+    const { email, password } = req.body;
     console.log('signin request', email);
-    if(!email || !password) {
+    if (!email || !password) {
         return res.status(400).json('incorrect form submission');
     }
     db.select('email', 'hash').from('login')
         .where('email', '=', email)
         .then(data => {
-            const isValid = bcrypt.compareSync(password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', email)
-                    .then(user => {
-                        console.log('signin success', user[0]);
-                        res.json(user[0])
-                    })
+            if (data.length) {
+                const isValid = bcrypt.compareSync(password, data[0].hash);
+                if (isValid) {
+                    return db.select('*').from('users')
+                        .where('email', '=', email)
+                        .then(user => {
+                            console.log('Signin success:', user[0]);
+                            res.json(user[0]);
+                        })
+                } else {
+                    console.log('Wrong credentials (bad password)');
+                    res.status(400).json('wrong credentials');
+                }
             } else {
-                res.status(400).json('wrong credentials')
+                console.log('Wrong credentials (no email found)');
+                res.status(400).json('wrong credentials');
             }
         })
-        .catch(err => res.status(400).json('unable to get credentials'))
-}
+        .catch(err => {
+            console.error('Signin failed:', err);
+            res.status(400).json('unable to signin');
+        });
+};
 
 module.exports = {
     handleSignin
